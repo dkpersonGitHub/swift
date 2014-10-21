cecc80cddd3ebe7996cfbf0df599e30ca8129abf
dukai <w55114676@126.com>
2014-10-21 22:55:37 +0800

    create demo

---
+++ swiftFirstCode/swiftFirstCodeTests/swiftFirstCodeTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftFirstCodeTests.swift
+//  swiftFirstCodeTests
+//
+//  Created by dukai on 14-10-21.
+//  Copyright (c) 2014年 dk. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swiftFirstCodeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
