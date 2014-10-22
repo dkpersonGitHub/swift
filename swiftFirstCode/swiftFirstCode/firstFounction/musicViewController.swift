cb855a9d49d23bcef5747594ca587330593c9b29
dukai <w55114676@126.com>
2014-10-22 21:57:07 +0800

    create tableView  and music

---
+++ swiftFirstCode/swiftFirstCode/firstFounction/musicViewController.swift
@@ -0,0 +1,35 @@
+//
+//  musicViewController.swift
+//  swiftFirstCode
+//
+//  Created by dukai on 14-10-22.
+//  Copyright (c) 2014年 dk. All rights reserved.
+//
+
+import UIKit
+
+class musicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swiftFirstCode/swiftFirstCode/macroHeader.swift
@@ -0,0 +1,13 @@
+//
+//  macroHeader.swift
+//  swiftFirstCode
+//
+//  Created by dukai on 14-10-22.
+//  Copyright (c) 2014年 dk. All rights reserved.
+//
+
+import UIKit
+
+class macroHeader: NSObject {
+   
+}
