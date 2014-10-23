f0ee8b602b0f5fc351946eb22c25cbbc54686bff
dukai <w55114676@126.com>
2014-10-23 23:44:19 +0800

    dukai

--- swiftFirstCode/swiftFirstCode/firstFounction/vedioViewController.swift
+++ swiftFirstCode/swiftFirstCode/firstFounction/vedioViewController.swift
@@ -7,29 +7,38 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class vedioViewController: UIViewController {
-
+  
+    var vedioPlayer:MPMoviePlayerController?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var path:String = NSBundle.mainBundle().resourcePath!
+        path += "/倒带.mp4"
+        var pathUrl = NSURL .fileURLWithPath(path)
+        vedioPlayer = MPMoviePlayerController(contentURL: pathUrl)
+        vedioPlayer?.view.backgroundColor = UIColor.blackColor()
+        vedioPlayer?.scalingMode = MPMovieScalingMode.AspectFit
+        
+        vedioPlayer?.view.frame = CGRectMake(0, 100, screen_width, 200)
+        vedioPlayer?.fullscreen = true
+        
+        self.view.addSubview(vedioPlayer!.view)
+       
     }
-
+    @IBAction func play()
+    {
+         vedioPlayer?.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
